d41c9591043c369614fea5c583771f21e2d04e29
Malfred Koryor <34602737+shadowpy@users.noreply.github.com>
2018-07-12 11:57:33 -0500

    Update Code.py

--- Code.py
+++ Code.py
@@ -1,4 +1,4 @@
-Selection_sort
+Insertion sort
 
 arr = [5,2,4,6,1,3]
 for j in range(0, len(arr)):
@@ -8,7 +8,7 @@ for j in range(0, len(arr)):
         arr[i+1] = arr[i]
         i = i - 1
     arr[i + 1] = key
-    
+#[1,2,3,4,5,6]
 
 
 Linear_search(nums, v):
@@ -18,5 +18,20 @@ Linear_search(nums, v):
       i = j
       return i
   return i
+
+print(Linear_search([1,2,4,3], 5))
+# None
   
-  
+
+Selection_sort
+numbers = [2,3,5,2,8]
+
+for i in range(len(numbers) - 1):
+    min = i 
+    for j in range(i + 1, len(numbers)):
+        if numbers[j] < numbers[min]:
+            min = j
+    numbers[min] = numbers[i]
+    
+#[2,2,3,5,8]
+
