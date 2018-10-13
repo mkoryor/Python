da8b88d9c8e81ef781a849a1225165b9d121ac95
Malfred Koryor <34602737+shadowpy@users.noreply.github.com>
2018-10-12 21:38:08 -0500

    Update Scrabble.py

--- Scrabble.py
+++ Scrabble.py
@@ -19,7 +19,7 @@ WORDLIST_FILENAME = "words.txt"
 
 def load_words():
     """
-    Returns a list of valid words. Words are strings of lowercase letters.
+    This returns a list of valid words. Words are strings of lowercase letters.
 
     Depending on the size of the word list, this function may
     take a while to finish.
