c3f93e0676c8932ca02d879f1efee2326f1bea48
Malfred Koryor <34602737+shadowpy@users.noreply.github.com>
2019-07-27 23:23:39 -0300

    Create UmbrellaReminder.py
    API that sends a text message to my phone to see if I need an umbrella for the bipolar Lubbock weather :)

---
+++ UmbrellaReminder.py
@@ -0,0 +1,41 @@
+#! python3
+# umbrellaReminder.py - check the weather to see if I need an umbrella 
+
+from twilio.rest import Client
+import requests, bs4
+from operator import itemgetter
+
+# rest API from twilio
+# Your Account SID from twilio.com/console
+account_sid = "AC7226356ef66d2de53d535237cba9e2dc"
+# Your Auth Token from twilio.com/console
+auth_token  = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+
+
+myNumber = '+1469XXXXXXX'
+client = Client(account_sid, auth_token)
+
+# create a message to myself
+def textmyself():
+
+
+	res = requests.get('https://weather.com/weather/today/l/c1535f42ba5fc52449e416514aca69b3b2a16aae4b89abd6c92e662f7a89c02f').text
+	soup = bs4.BeautifulSoup(res, features='html.parser')
+
+	state = soup.find('header', class_='loc-container').text
+	fullforcast = soup.find('div', class_='today_nowcard-section today_nowcard-condition').text
+	
+
+	message = client.messages.create(
+	    to="+14695104352", 
+	    from_="+17033489722",
+	    body= fullforcast)
+
+	return message.sid 
+
+#print(textmyself())
+
+
+
+#print(weather.prettify())
+
