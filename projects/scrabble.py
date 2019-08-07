a83166c7677be1266d69f86b1752b4d0c7806bb5
Malfred Koryor <34602737+shadowpy@users.noreply.github.com>
2019-08-07 09:19:23 -0500

    Create scrabble.py

---
+++ projects/scrabble.py
@@ -0,0 +1,283 @@
+
+import random
+import string
+
+VOWELS = 'aeiou'
+CONSONANTS = 'bcdfghjklmnpqrstvwxyz'
+HAND_SIZE = 7
+
+SCRABBLE_LETTER_VALUES = {
+    'a': 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1, 'f': 4, 'g': 2, 'h': 4, 'i': 1, 'j': 8, 'k': 5, 'l': 1, 'm': 3, 'n': 1, 'o': 1, 'p': 3, 'q': 10, 'r': 1, 's': 1, 't': 1, 'u': 1, 'v': 4, 'w': 4, 'x': 8, 'y': 4, 'z': 10
+}
+
+# -----------------------------------
+# Helper code
+# (you don't need to understand this helper code)
+
+WORDLIST_FILENAME = "words.txt"
+
+
+def load_words():
+    """
+    This returns a list of valid words. Words are strings of lowercase letters.
+    Depending on the size of the word list, this function may
+    take a while to finish.
+    """
+    print ("Loading word list from file...")
+    # inFile: file
+    inFile = open(WORDLIST_FILENAME, 'r')
+    # wordlist: list of strings
+    wordlist = []
+    for line in inFile:
+        wordlist.append(line.strip().lower())
+    print ("  ", len(wordlist), "words loaded.")
+    return wordlist
+
+
+def get_frequency_dict(sequence):
+    """
+    Returns a dictionary where the keys are elements of the sequence
+    and the values are integer counts, for the number of times that
+    an element is repeated in the sequence.
+    sequence: string or list
+    return: dictionary
+    """
+    # freqs: dictionary (element_type -> int)
+    freq = {}
+    for x in sequence:
+        freq[x] = freq.get(x, 0) + 1
+    return freq
+
+
+# (end of helper code)
+# -----------------------------------
+
+#
+# Scoring
+#
+
+
+# Malfred Koryor
+def get_word_score(word, n):
+    """
+    Returns the score for a word. Assumes the word is a
+    valid word.
+    The score for a word is the sum of the points for letters
+    in the word, plus 50 points if all n letters are used on
+    the first go.
+    Letters are scored as in Scrabble; A is worth 1, B is
+    worth 3, C is worth 3, D is worth 2, E is worth 1, and so on.
+    word: string (lowercase letters)
+    returns: int >= 0
+    """
+    # TO DO ...
+    stash = 0
+
+    for letter in word:
+        if letter == ' ':
+            return 0
+        stash = 0
+
+        for char in word:
+            stash = stash + SCRABBLE_LETTER_VALUES[char]
+
+    if len(word) == n:
+        stash = stash + 50
+    return stash
+
+#
+# Make sure you understand how this function works and what it does!
+#
+
+
+def display_hand(hand):
+    """
+    Displays the letters currently in the hand.
+    For example:
+       display_hand({'a':1, 'x':2, 'l':3, 'e':1})
+    Should print out something like:
+       a x x l l l e
+    The order of the letters is unimportant.
+    hand: dictionary (string -> int)
+    """
+    for letter in hand.keys():
+        for j in range(hand[letter]):
+            print (letter,)             # print all on the same line
+    print                              # print an empty line
+
+#
+# Make sure you understand how this function works and what it does!
+#
+
+
+def deal_hand(n):
+    """
+    Returns a random hand containing n lowercase letters.
+    At least n/3 the letters in the hand should be VOWELS.
+    Hands are represented as dictionaries. The keys are
+    letters and the values are the number of times the
+    particular letter is repeated in that hand.
+    n: int >= 0
+    returns: dictionary (string -> int)
+    """
+    hand = {}
+    num_vowels = int(n / 3)
+
+    for i in range(num_vowels):
+        x = VOWELS[random.randrange(0, len(VOWELS))]
+        hand[x] = hand.get(x, 0) + 1
+
+    for i in range(num_vowels, n):
+        x = CONSONANTS[random.randrange(0, len(CONSONANTS))]
+        hand[x] = hand.get(x, 0) + 1
+
+    return hand
+
+#
+# Represent a hand
+#
+# Malfred Koryor
+
+
+def update_hand(hand, word):
+    """
+    Assumes that 'hand' has all the letters in word.
+    In other words, this assumes that however many times
+    a letter appears in 'word', 'hand' has at least as
+    many of that letter in it.
+    Updates the hand: uses up the letters in the given word
+    and returns the new hand, without those letters in it.
+    Has no side effects: does not mutate hand.
+    word: string
+    hand: dictionary (string -> int)
+    returns: dictionary (string -> int)
+    """
+    # TO DO ...
+    n_h = hand.copy()
+    for i in word:
+        if i in n_h:
+            n_h[i] = n_h[i] - 1
+    return n_h
+
+#
+# Validity of word
+#
+
+# Malfred Koryor
+
+
+def is_valid_word(word, hand, word_list):
+    """
+    Returns True if word is in the word_list and is entirely
+    composed of letters in the hand. Otherwise, returns False.
+    Does not mutate hand or word_list.
+    word: string
+    hand: dictionary (string -> int)
+    word_list: list of lowercase strings
+    """
+    # TO DO ...
+
+    n_h = hand.copy()
+    found = False
+
+    if word not in word_list:
+        return found
+    n_h2 = n_h
+
+    for c in word:
+        if n_h.get(c, 0) > 0:
+            n_h[c] = n_h[c] - 1
+        else:
+            return found
+    found = True
+    return found
+
+
+#
+# Playing a hand
+#
+
+# Malfred Koryor
+def play_hand(hand, word_list):
+    """
+    Allows the user to play the given hand, as follows:
+    * The hand is displayed.
+    * The user may input a word.
+    * An invalid word is rejected, and a message is displayed asking
+      the user to choose another word.
+    * When a valid word is entered, it uses up letters from the hand.
+    * After every valid word: the score for that word and the total
+      score so far are displayed, the remaining letters in the hand
+      are displayed, and the user is asked to input another word.
+    * The sum of the word scores is displayed when the hand finishes.
+    * The hand finishes when there are no more unused letters.
+      The user can also finish playing the hand by inputing a single
+      period (the string '.') instead of a word.
+    * The final score is displayed.
+      hand: dictionary (string -> int)
+      word_list: list of lowercase strings
+    """
+    # TO DO ...
+    starting_score = 0
+
+    while HAND_SIZE > 0:
+        display_hand(hand)
+
+        creation = input("Spell a word or if there's no words type a (dot) '.' to end game: ")
+        if creation == '.':
+            print("Game Over!")
+            break
+        else:
+            if not is_valid_word(creation, hand, word_list):
+                print("That is an invalid word buddy")
+            else:
+                word_score = get_word_score(creation, hand)
+                starting_score = starting_score + word_score
+                print("your word was " + creation + " which give you a score of " + str(word_score) + " giving you a whooping points " + str(starting_score))
+
+                hand = update_hand(hand, creation)
+
+    print("Thank you for playing the best game ever, you received " + str(starting_score) + " points")
+
+    #
+    # The game
+    # Make sure you understand how this code works!
+    #
+
+    # Malfred Koryor
+
+
+def play_game(word_list):
+    """
+    Allow the user to play an arbitrary number of hands.
+    * Asks the user to input 'n' or 'r' or 'e'.
+    * If the user inputs 'n', let the user play a new (random) hand.
+      When done playing the hand, ask the 'n' or 'e' question again.
+    * If the user inputs 'r', let the user play the last hand again.
+    * If the user inputs 'e', exit the game.
+    * If the user inputs anything else, ask them again.
+    """
+    # TO DO ...
+
+    hand = deal_hand(HAND_SIZE)  # random init
+    while True:
+        cmd = input('Enter n to deal a new hand, r to replay the last hand, or e to end game: ')
+        if cmd == 'n':
+            hand = deal_hand(HAND_SIZE)
+            play_hand(hand.copy(), word_list)
+            print()
+        elif cmd == 'r':
+            play_hand(hand.copy(), word_list)
+            print()
+        elif cmd == 'e':
+            break
+        else:
+            print ("Invalid command.")
+
+
+#
+# Build data structures used for entire session and play game
+#
+if __name__ == '__main__':
+    word_list = load_words()
+    play_game(word_list)
