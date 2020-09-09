509bfdc3f1a3db588a3cc433face7be9e3960433
Malfred Koryor <mkoryor@gmail.com>
2020-09-09 01:59:04 -0500

    Update cloneEvenNumbers.py

--- arrays & strings I/cloneEvenNumbers.py
+++ arrays & strings I/cloneEvenNumbers.py
@@ -1,6 +1,6 @@
 
 
-''' (E) Given an array of numbers, replace each even number with 
+''' (Reverse Traversal [E]) Given an array of numbers, replace each even number with 
 two of the same number. e.g, [1,2,5,6,8] -> [1,2,2,5,6,6,8,8]. Assume that
 the array has enough space to accommodate the result '''
 
