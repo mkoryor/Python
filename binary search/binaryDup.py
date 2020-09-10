49c15511af3df6fc8c75c97ff8a2daefd2c4b882
Malfred Koryor <mkoryor@gmail.com>
2020-09-10 01:16:30 -0500

    Update binaryDup.py

--- binary search/binaryDup.py
+++ binary search/binaryDup.py
@@ -4,7 +4,7 @@
 '''(Binary Search Duplicates Technique [E]): Given a sorted array that can contain duplicates, find the first 
 occurrence of a target element T.For example, if A = [2,3,4,4,5,6] and T = 4, return index 2.'''
 
-def binaryDup(arr, x):
+def binaryDup(arr, T):
 
     low = 0
     high = len(arr) - 1
@@ -12,10 +12,10 @@ def binaryDup(arr, x):
     while low <= high:
         mid = low + (high - low) // 2
 
-        if arr[mid] > x or arr[mid] == x and mid > 0 and arr[mid - 1] == x:
+        if arr[mid] > T or arr[mid] == T and mid > 0 and arr[mid - 1] == T:
             high = mid - 1
         
-        elif arr[mid] < x:
+        elif arr[mid] < T:
             low = mid + 1
             
         else:
