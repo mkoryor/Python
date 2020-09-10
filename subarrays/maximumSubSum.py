c457799c0ee617029713e6da918dfb97ed651b5d
Malfred Koryor <mkoryor@gmail.com>
2020-09-09 21:45:24 -0500

    Create maximumSubSum.py

---
+++ subarrays/maximumSubSum.py
@@ -0,0 +1,33 @@
+
+
+
+
+'''(Kadane's Algorithm): Given an array of integers, find the contiguous subarray 
+(with at least 1 element) with the maximum sum. The array can contain both negative 
+and positive integers. For example:  [1,2,-1,2,-3,2,-5]  -> first 4 elements have the 
+largest sum. Return (0,3)'''
+
+def maximumSubSum(arr):
+    
+    maxSum = arr[0]
+    maxEndingHere = arr[0]
+    i = 0
+
+    while i <= len(arr) - 1:
+        maxEndingHere = max(maxEndingHere + arr[i], arr[i])
+        maxSum = max(maxSum, maxEndingHere)
+        
+        i += 1
+
+    return maxSum
+
+print(maximumSumSub([1,2,-1,2,-3,2, -5]))
+
+
+# Output: 5 - sub = [1,2,-1,2]
+# Time: O(n) Space: O(1)
+
+
+
+
+
