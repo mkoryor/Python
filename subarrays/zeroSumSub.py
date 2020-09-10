742fbf9c8149105232ee0b1ff996a3c897a13ef7
Malfred Koryor <mkoryor@gmail.com>
2020-09-10 00:23:35 -0500

    Update zeroSumSub.py

--- subarrays/zeroSumSub.py
+++ subarrays/zeroSumSub.py
@@ -2,7 +2,7 @@
 
 
 
-'''(Prefix Sums Technique [M]):(Level: Medium) Given an array of integers, 
+'''(Prefix Sums Technique [M]): Given an array of integers, 
 find the contiguous subarray that sums to 0. The array can contain both negative and 
 positive integers.For example: Input = [2,4,-2,1,-3,5,-3], Result = [4,-2,1,-3]'''
 
