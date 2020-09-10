41641a649f1554541fdf40437a3f379fdbd3991c
Malfred Koryor <mkoryor@gmail.com>
2020-09-10 01:51:03 -0500

    Update cyclicSortedMin.py

--- binary search/cyclicSortedMin.py
+++ binary search/cyclicSortedMin.py
@@ -4,7 +4,7 @@
 
 
 
-'''(Binary Search Special Trick Cyclic Sorted Array [E]): 1. (Level: Easy) Given a sorted array A that has 
+'''(Binary Search Special Trick Cyclic Sorted Array [E]): Given a sorted array A that has 
 been rotated in a cycle, find the smallest element of the array in O(log(n)) time. For example,
 [1,2,4,5,7,8] rotated by 3 gives us A = [5,7,8,1,2,4] and the smallest number is 1.
 [1,2,4,5,7,8] rotated by 1 gives us A = [8,1,2,4,5,7] and the smallest number is 1.'''
