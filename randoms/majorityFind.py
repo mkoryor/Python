0db44b0291788b4ac24c5ef25dd47ec3540c01cf
Malfred Koryor <mkoryor@gmail.com>
2020-09-13 02:57:55 -0500

    Create majorityFind.py

---
+++ randoms/majorityFind.py
@@ -0,0 +1,34 @@
+
+
+
+
+
+def majorityFind(arr):
+
+    candidate = arr[0]
+    count = 1
+
+    for i in range(len(arr)):
+        if arr[i] == candidate:
+            count += 1
+
+        elif count > 0:
+            count -= 1
+        
+        else:
+            candidate = arr[i]
+            count = 1   
+
+    # check arr, verify candidate is majority
+    count = 0
+    for i in range(len(arr)):
+        if arr[i] == candidate:
+            count += 1
+    
+    return candidate if count > len(arr) // 2 else None
+
+print(majorityFind([1,2,4,1,1]))
+
+
+
+
