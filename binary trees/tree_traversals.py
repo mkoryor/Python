56fa467e6b7fb877d37447b59cf0f484e396dc8c
Malfred Koryor <mkoryor@gmail.com>
2021-04-20 15:23:32 -0500

    Update tree_traversals.py

--- binary trees/tree_traversals.py
+++ binary trees/tree_traversals.py
@@ -60,10 +60,10 @@ def printPreorder(root):
   
 # Driver code 
 root = Node(1) 
-root.left      = Node(2) 
-root.right     = Node(3) 
-root.left.left  = Node(4) 
-root.left.right  = Node(5) 
+root.left = Node(2) 
+root.right = Node(3) 
+root.left.left = Node(4) 
+root.left.right = Node(5) 
 print "Preorder traversal of binary tree is"
 printPreorder(root) 
   
