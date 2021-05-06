cda7be0342e67e4b6b3bff7a2e9be0ef5f05562e
Malfred Koryor <mkoryor@gmail.com>
2021-05-05 23:51:18 -0500

    Update trie_imp.py

--- trie/trie_imp.py
+++ trie/trie_imp.py
@@ -1,93 +1,87 @@
 
 
+from collections import OrderedDict
 
-# Python program for insert and search 
-# operation in a Trie 
-  
-class TrieNode: 
+
+class Node(object):
+
+    def __init__(self, key, parent=None, terminates=False):
+        self.key = key
+        self.terminates = False
+        self.parent = parent
+        self.children = {}
+
+
+class Trie(object):
+
+    def __init__(self):
+        self.root = Node('')
+
+    def find(self, word):
+        if word is None:
+            raise TypeError('word cannot be None')
+        node = self.root
+        for char in word:
+            if char in node.children:
+                node = node.children[char]
+            else:
+                return None
+        return node if node.terminates else None
+
       
-    # Trie node class 
-    def __init__(self): 
-        self.children = [None]*26
-  
-        # isEndOfWord is True if node represent the end of the word 
-        self.isEndOfWord = False
-  
-class Trie: 
       
-    # Trie data structure class 
-    def __init__(self): 
-        self.root = self.getNode() 
-  
-    def getNode(self): 
+    def insert(self, word):
+        if word is None:
+            raise TypeError('word cannot be None')
+        node = self.root
+        parent = None
+        for char in word:
+            if char in node.children:
+                node = node.children[char]
+            else:
+                node.children[char] = Node(char, parent=node)
+                node = node.children[char]
+        node.terminates = True
+
+        
+        
+    def remove(self, word):
+        if word is None:
+            raise TypeError('word cannot be None')
+        node = self.find(word)
+        if node is None:
+            raise KeyError('word does not exist')
+        node.terminates = False
+        parent = node.parent
+        while parent is not None:
+            # As we are propagating the delete up the 
+            # parents, if this node has children, stop
+            # here to prevent orphaning its children.
+            # Or
+            # if this node is a terminating node that is
+            # not the terminating node of the input word, 
+            # stop to prevent removing the associated word.
+            if node.children or node.terminates:
+                return
+            del parent.children[node.key]
+            node = parent
+            parent = parent.parent
+
+            
+            
+    def list_words(self):
+        result = []
+        curr_word = ''
+        self._list_words(self.root, curr_word, result)
+        return result
+
       
-        # Returns new trie node (initialized to NULLs) 
-        return TrieNode() 
-  
-    def _charToIndex(self,ch): 
-          
-        # private helper function 
-        # Converts key current character into index 
-        # use only 'a' through 'z' and lower case 
-          
-        return ord(ch)-ord('a') 
-  
-  
-    def insert(self,key): 
-          
-        # If not present, inserts key into trie 
-        # If the key is prefix of trie node,  
-        # just marks leaf node 
-        pCrawl = self.root 
-        length = len(key) 
-        for level in range(length): 
-            index = self._charToIndex(key[level]) 
-  
-            # if current character is not present 
-            if not pCrawl.children[index]: 
-                pCrawl.children[index] = self.getNode() 
-            pCrawl = pCrawl.children[index] 
-  
-        # mark last node as leaf 
-        pCrawl.isEndOfWord = True
-  
-    def search(self, key): 
-          
-        # Search key in the trie 
-        # Returns true if key presents  
-        # in trie, else false 
-        pCrawl = self.root 
-        length = len(key) 
-        for level in range(length): 
-            index = self._charToIndex(key[level]) 
-            if not pCrawl.children[index]: 
-                return False
-            pCrawl = pCrawl.children[index] 
-  
-        return pCrawl != None and pCrawl.isEndOfWord 
-  
-# driver function 
-def main(): 
-  
-    # Input keys (use only 'a' through 'z' and lower case) 
-    keys = ["the","a","there","anaswe","any", 
-            "by","their"] 
-    output = ["Not present in trie", 
-              "Present in trie"] 
-  
-    # Trie object 
-    t = Trie() 
-  
-    # Construct trie 
-    for key in keys: 
-        t.insert(key) 
-  
-    # Search for different keys 
-    print("{} ---- {}".format("the",output[t.search("the")])) 
-    print("{} ---- {}".format("these",output[t.search("these")])) 
-    print("{} ---- {}".format("their",output[t.search("their")])) 
-    print("{} ---- {}".format("thaw",output[t.search("thaw")])) 
-  
-if __name__ == '__main__': 
-    main() 
+      
+    def _list_words(self, node, curr_word, result):
+        if node is None:
+            return
+        for key, child in node.children.items():
+            if child.terminates:
+                result.append(curr_word + key)
+            self._list_words(child, curr_word + key, result)
   
