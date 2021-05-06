319875b4be1fb8de89aca9916e17f7bd4de02b06
Malfred Koryor <mkoryor@gmail.com>
2021-05-06 12:58:54 -0500

    Add files via upload

---
+++ coding patterns/miscellaneous/k_smallest_number_BF_using_sorting.py
@@ -0,0 +1,47 @@
+
+
+
+
+
+"""
+
+[H] Given an unsorted array of numbers, find Kth smallest number in it.
+Please note that it is the Kth smallest number in the sorted order, not the Kth distinct element.
+
+Example 1:
+
+Input: [1, 5, 12, 2, 11, 5], K = 3
+Output: 5
+Explanation: The 3rd smallest number is '5', as the first two smaller numbers are [1, 2].
+Example 2:
+
+Input: [1, 5, 12, 2, 11, 5], K = 4
+Output: 5
+Explanation: The 4th smallest number is '5', as the first three smaller numbers are
+[1, 2, 5].
+"""
+
+
+
+
+
+
+# Time: O(NlogN) Space: O(N)
+def find_Kth_smallest_number(nums, k):
+  return sorted(nums)[k-1]
+
+
+def main():
+
+  print("Kth smallest number is: " +
+        str(find_Kth_smallest_number([1, 5, 12, 2, 11, 5], 3)))
+
+  # since there are two 5s in the input array, our 3rd and 4th smallest numbers should be a '5'
+  print("Kth smallest number is: " +
+        str(find_Kth_smallest_number([1, 5, 12, 2, 11, 5], 4)))
+
+  print("Kth smallest number is: " +
+        str(find_Kth_smallest_number([5, 12, 11, -1, 12], 3)))
+
+
+main()
