c9d0b2f0dd8c4e2aa3042e6bc98be08cbd53cc4b
Malfred Koryor <mkoryor@gmail.com>
2021-05-06 12:01:36 -0500

    Create bfs_tree_traversal_imp.py

---
+++ binary trees/bfs_tree_traversal_imp.py
@@ -0,0 +1,57 @@
+
+
+
+
+from collections import deque
+
+
+class BstBfs(Bst):
+
+    def bfs(self, visit_func):
+        if self.root is None:
+            raise TypeError('root is None')
+        queue = deque()
+        queue.append(self.root)
+        
+        while queue:
+            node = queue.popleft()
+            visit_func(node)
+            if node.left is not None:
+                queue.append(node.left)
+            if node.right is not None:
+                queue.append(node.right)
+                
+                
+                
+ import unittest
+
+
+class TestBfs(unittest.TestCase):
+
+    def __init__(self, *args, **kwargs):
+        super(TestBfs, self).__init__()
+        self.results = Results()
+
+    def test_bfs(self):
+        bst = BstBfs(Node(5))
+        bst.insert(2)
+        bst.insert(8)
+        bst.insert(1)
+        bst.insert(3)
+        bst.bfs(self.results.add_result)
+        self.assertEqual(str(self.results), '[5, 2, 8, 1, 3]')
+
+        print('Success: test_bfs')
+
+
+def main():
+    test = TestBfs()
+    test.test_bfs()
+
+
+if __name__ == '__main__':
+    main()
+    
+    
+    
+    
