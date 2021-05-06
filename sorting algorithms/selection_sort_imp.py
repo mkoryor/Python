f7c3b15c5c01e55334052e2cacad9b4086e1f581
Malfred Koryor <mkoryor@gmail.com>
2021-05-06 11:35:58 -0500

    Create selection_sort_imp.py

---
+++ sorting algorithms/selection_sort_imp.py
@@ -0,0 +1,105 @@
+
+
+class SelectionSort(object):
+
+    def sort(self, data):
+        if data is None:
+            raise TypeError('data cannot be None')
+        if len(data) < 2:
+            return data
+        for i in range(len(data) - 1):
+            min_index = i
+            for j in range(i + 1, len(data)):
+                if data[j] < data[min_index]:
+                    min_index = j
+            if data[min_index] < data[i]:
+                data[i], data[min_index] = data[min_index], data[i]
+        return data
+
+    def sort_iterative_alt(self, data):
+        if data is None:
+            raise TypeError('data cannot be None')
+        if len(data) < 2:
+            return data
+        for i in range(len(data) - 1):
+            self._swap(data, i, self._find_min_index(data, i))
+        return data
+
+    def sort_recursive(self, data):
+        if data is None:
+            raise TypeError('data cannot be None')
+        if len(data) < 2:
+            return data
+        return self._sort_recursive(data, start=0)
+
+    def _sort_recursive(self, data, start):
+        if data is None:
+            return
+        if start < len(data) - 1:
+            swap(data, start, self._find_min_index(data, start))
+            self._sort_recursive(data, start + 1)
+        return data
+
+    def _find_min_index(self, data, start):
+        min_index = start
+        for i in range(start + 1, len(data)):
+            if data[i] < data[min_index]:
+                min_index = i
+        return min_index
+
+    def _swap(self, data, i, j):
+        if i != j:
+            data[i], data[j] = data[j], data[i]
+        return data
+      
+      
+      
+      
+
+      
+      
+      
+import unittest
+
+
+class TestSelectionSort(unittest.TestCase):
+
+    def test_selection_sort(self, func):
+        print('None input')
+        self.assertRaises(TypeError, func, None)
+
+        print('Empty input')
+        self.assertEqual(func([]), [])
+
+        print('One element')
+        self.assertEqual(func([5]), [5])
+
+        print('Two or more elements')
+        data = [5, 1, 7, 2, 6, -3, 5, 7, -10]
+        self.assertEqual(func(data), sorted(data))
+
+        print('Success: test_selection_sort\n')
+
+
+def main():
+    test = TestSelectionSort()
+    selection_sort = SelectionSort()
+    test.test_selection_sort(selection_sort.sort)
+    try:
+        test.test_selection_sort(selection_sort.sort_recursive)
+        test.test_selection_sort(selection_sort.sor_iterative_alt)
+    except NameError:
+        # Alternate solutions are only defined
+        # in the solutions file
+        pass
+
+
+if __name__ == '__main__':
+    main()
+    
+    
+    
+    
+    
+    
+    
